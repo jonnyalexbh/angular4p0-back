18215f6582837c7ebcd1affff54df778fcdaccce
jonnyalexbh <jonnyalex.bh@gmail.com>
2017-09-01 08:18:20 -0500

    list, view, update, delete products

--- bbdd.sql
+++ bbdd.sql
@@ -4,7 +4,7 @@ USE angular4p0;
 CREATE TABLE productos(
 id			int(255) auto_increment not null,
 nombre 		varchar(255),
-description text,
+descripcion text,
 precio		varchar(255),
 imagen		varchar(255),
 CONSTRAINT pk_productos PRIMARY KEY(id)
--- index.php
+++ index.php
@@ -14,6 +14,103 @@ $app->get("/employees", function () use ($app){
   echo "Hello employees";
 });
 
+// LIST ALL PRODUCTS
+$app->get('/products', function() use($db, $app){
+  $sql = 'SELECT * FROM productos ORDER BY id DESC;';
+  $query = $db->query($sql);
+
+  $productos = array();
+  while ($producto = $query->fetch_assoc()) {
+    $productos[] = $producto;
+  }
+
+  $result = array(
+    'status' => 'success',
+    'code'	 => 200,
+    'data' => $productos
+  );
+
+  echo json_encode($result);
+});
+
+// RETURN ONE PRODUCT
+$app->get('/products/:id', function($id) use($db, $app){
+  $sql = 'SELECT * FROM productos WHERE id = '.$id;
+  $query = $db->query($sql);
+
+  $result = array(
+    'status' 	=> 'error',
+    'code'		=> 404,
+    'message' 	=> 'Producto no disponible'
+  );
+
+  if($query->num_rows == 1){
+    $producto = $query->fetch_assoc();
+
+    $result = array(
+      'status' 	=> 'success',
+      'code'		=> 200,
+      'data' 	=> $producto
+    );
+  }
+
+  echo json_encode($result);
+});
+
+// REMOVE A PRODUCT
+$app->get('/delete-product/:id', function($id) use($db, $app){
+  $sql = 'DELETE FROM productos WHERE id = '.$id;
+  $query = $db->query($sql);
+
+  if($query){
+    $result = array(
+      'status' 	=> 'success',
+      'code'		=> 200,
+      'message' 	=> 'El producto se ha eliminado correctamente!!'
+    );
+  }else{
+    $result = array(
+      'status' 	=> 'error',
+      'code'		=> 404,
+      'message' 	=> 'El producto no se ha eliminado!!'
+    );
+  }
+
+  echo json_encode($result);
+});
+
+// UPDATE A PRODUCT
+$app->post('/update-product/:id', function($id) use($db, $app){
+  $json = $app->request->post('json');
+  $data = json_decode($json, true);
+
+  $sql = "UPDATE productos SET ".
+  "nombre = '{$data["nombre"]}', ".
+  "descripcion = '{$data["descripcion"]}', ".
+  "precio = '{$data["precio"]}' WHERE id = {$id}";
+
+  $query = $db->query($sql);
+  
+  if($query){
+    $result = array(
+      'status' 	=> 'success',
+      'code'		=> 200,
+      'message' 	=> 'El producto se ha actualizado correctamente!!'
+    );
+  }else{
+    $result = array(
+      'status' 	=> 'error',
+      'code'		=> 404,
+      'message' 	=> 'El producto no se ha actualizado!!'
+    );
+  }
+
+  echo json_encode($result);
+
+});
+
+// UPLOAD AN IMAGE TO A PRODUCT
+
 // SAVE PRODUCTS
 $app->post('/products', function() use($app, $db){
 
@@ -42,7 +139,7 @@ $app->post('/products', function() use($app, $db){
   "'{$data['precio']}',".
   "'{$data['imagen']}'".
   ");";
-  
+
   $insert = $db->query($query);
 
   $result = array(
